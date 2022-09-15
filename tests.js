ccb9989383d6a404e32e2144f2630cc2ed44b758
Sebastian Webster <coolman16738@gmail.com>
2022-09-15 16:18:23 +1200

    testing github actions

--- tests.js
+++ tests.js
@@ -1,2 +1,3 @@
 console.log('Tests are coming soon...')
 console.log('This is just to see if tests pass and run on each push to GitHub...')
+throw new Error('This is to see if GitHub actions will fail if node throws an error')
