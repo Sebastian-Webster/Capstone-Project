911c2cbdac1f317740a374a902c4c849111fd320
Sebastian Webster <coolman16738@gmail.com>
2022-09-15 16:30:59 +1200

    add test for frontend/useSharedCode.calculateDifferenceBetweenNowAndUTCMillisecondsTime

--- tests.js
+++ tests.js
@@ -1,3 +1,15 @@
-console.log('Tests are coming soon...')
-console.log('This is just to see if tests pass and run on each push to GitHub...')
-throw new Error('This is to see if GitHub actions will fail if node throws an error')
+import useSharedCode from './frontend/src/hooks/useSharedCode'
+const calculateDifferenceBetweenNowAndUTCMillisecondsTime = useSharedCode().calculateDifferenceBetweenNowAndUTCMillisecondsTime
+
+let testRuns;
+console.log('Starting tests...')
+console.log('Starting frontend useSharedCode.calculateDifferenceBetweenNowAndUTCMillisecondsTime tests (testing to see if the result is a string and that there are no 0s at the start of the string)')
+testRuns = 999_999_999
+for (let i = 0; i < testRuns; i++) {
+    const randomNumber = Math.random() * 10 ^ 16
+    const result = calculateDifferenceBetweenNowAndUTCMillisecondsTime(randomNumber)
+    if (typeof result !== 'string') throw new Error('calculateDifferenceBetweenNowAndUTCMillisecondsTime is not a string! Result: ' + result + '. Random number was: ' + randomNumber)
+    if (result.charAt(0) === '0') throw new Error('There was a 0 at the start of the string returned by calculateDifferenceBetweenNowAndUTCMillisecondsTime! Result: ' + result + '. Random number was: ' + randomNumber)
+    if (i % 10_000_000 === 0) console.log('Ran ' + i + 'th test.')
+}
+console.log('Test was successful')
